8d2ae526ed4d7270036396a9a1691ec16ae1d7bb
Christof Damian <christof@damian.net>
2013-08-12 16:14:20 +0200

    colour git prompt

--- modules/development/files/git-profile.sh
+++ modules/development/files/git-profile.sh
@@ -1,5 +1,8 @@
 source /usr/share/git-core/contrib/completion/git-prompt.sh
 
 export PS1='[\u@\h \W$(declare -F __git_ps1 &>/dev/null && __git_ps1 " (%s)")]\$ '
+export PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
+
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
+export GIT_PS1_SHOWCOLORHINTS=true
