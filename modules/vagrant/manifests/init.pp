11df95247524c47c593b21a4d4152fab697cb15c
Christof Damian <christof@damian.net>
2013-11-15 10:31:58 +0100

    better vagrant install

--- modules/vagrant/manifests/init.pp
+++ modules/vagrant/manifests/init.pp
@@ -1,18 +1,19 @@
 class vagrant {
 
   $url = 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/vagrant_1.3.5_x86_64.rpm'
-  $tmp = '/tmp/vagrant.rpm'
+  $tmp = '/root/vagrant_1.3.5_x86_64.rpm'
 
   exec { 'download_vagrant':
     command => "/usr/bin/wget '$url' -O '$tmp'",
     creates => $tmp,
     require => Package['wget'],
-    notify => Exec['install_vagrant'],
+    notify => Package['vagrant'],
   }
 
-  exec { 'install_vagrant':
-    command => "/usr/bin/rpm -U '$tmp'",
-    refreshonly => true,
+  package { 'vagrant':
+    source => $tmp,
+    provider => 'rpm',
+    ensure => installed,
     require => Package['VirtualBox'],
   }
 
